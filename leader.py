f0fd4bf554537a7601225fbfa47eca2f55f6d74b
Rory McStay <rory@rorymcstay.com>
2020-05-24 16:02:12 +0100

    bumping req and improve logging

--- leader.py
+++ leader.py
@@ -1,24 +1,18 @@
 import logging
 import os
+from logging.config import dictConfig
 import argparse
 from feed.service import Client
 from feed.actionchains import KafkaActionSubscription, KafkaActionPublisher
 from feed.crawling import BrowserService, BrowserActions
 start = logging.getLogger("startup")
 
-
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-logging.FileHandler('/var/tmp/myapp.log')
-logging.getLogger("requests").setLevel(logging.WARNING)
-logging.getLogger("kafka").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.ERROR)
-logging.getLogger("selenium").setLevel(logging.WARNING)
-
-
 class LeaderCrawler(KafkaActionSubscription, BrowserService, KafkaActionPublisher):
 
     def __init__(self):
-        KafkaActionSubscription.__init__(self, topic='leader-route', implementation=BrowserActions)
+        queue = f'{os.getenv("KAFKA_TOPIC_PREFIX", "u")}-leader-queue'
+        logging.info(f'subscribing to {queue}')
+        KafkaActionSubscription.__init__(self, topic=queue, implementation=BrowserActions)
         BrowserService.__init__(self)
         KafkaActionPublisher.__init__(self)
 
@@ -41,6 +35,25 @@ class LeaderCrawler(KafkaActionSubscription, BrowserService, KafkaActionPublishe
 
 
 if __name__ == "__main__":
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s]%(thread)d: %(module)s - %(levelname)s - %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("kafka").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
+    logging.getLogger("selenium").setLevel(logging.WARNING)
     logging.info("\n".join([f'{key}={os.environ[key]}' for key in os.environ]))
     feed = LeaderCrawler()
     feed.main()
